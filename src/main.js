7a41911779f641df2bc5bde737c2861fbb2101d8
Darthwiings <105502133+Darthwiings@users.noreply.github.com>
2023-12-22 15:36:25 -0600

    Added animations

--- src/main.js
+++ src/main.js
@@ -11,4 +11,13 @@ import "../styles/utils.css";
 
 import darkMode from "./utils/themeSwitcher";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// init AOS animation
+AOS.init({
+  duration: 1500,
+  offset: 100,
+});
+
 darkMode();
